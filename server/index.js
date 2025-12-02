1d75bca7c7ac03f0cee6f659003feb2dc07dc3d8
Artema-cooler <agrushch@gmail.com>
2025-12-02 12:45:16 +0400

    Update index.js

--- server/index.js
+++ server/index.js
@@ -59,7 +59,7 @@ function pushPlayerToQueue(socket) {
         players[unmatchedPlayerId].opponent = socket.id;
         players[unmatchedPlayerId].symbol = (Math.random() > 0.5) ? "X" : "O";
         players[socket.id].symbol = (players[unmatchedPlayerId].symbol == "X") ? "O" : "X";
-        console.log("Match found: " + socket.id + " and " + umatchedPlayerId);
+        console.log("Match found: " + socket.id + " and " + unmatchedPlayerId);
         unmatchedPlayerId = null;
     } else {
         unmatchedPlayerId = socket.id;
